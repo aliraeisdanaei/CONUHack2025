e04f042332125f10a9a5dbf1cc64926d57eb0c5e
aliraeisdanaei <mrrookie2@gmail.com>
2025-02-01 21:10:03 -0500

    change the port name

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from current_optimise import process_data, simulate_deployment
 from Routes.fireReport import fireReport  # Import the Blueprint, not the function
 
@@ -12,3 +14,8 @@ CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+if name == 'main':
+    # Use Render's PORT environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
