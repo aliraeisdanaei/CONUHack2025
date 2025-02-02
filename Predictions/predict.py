8d8e5087f0d06c18d8ad9122b39a2eba52e6f8b7
aliraeisdanaei <mrrookie2@gmail.com>
2025-02-02 00:29:44 -0500

    improve model to not overfit for no fires

--- Predictions/predict.py
+++ Predictions/predict.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 from datetime import timedelta
 
 # 1. Load the historical data
@@ -40,13 +40,34 @@ def mark_fire_occurrence(fire_row):
 # Apply the labeling for each wildfire event.
 fire_data.apply(mark_fire_occurrence, axis=1)
 
+num_fires = (env_data['fire_occurrence'] == 1).sum()
+num_non_fires = 3 * num_fires
+print('Num of fire occurrences:', num_fires)
+print('Num of no fire occurences kept:', num_non_fires)
+
+# Filter rows where 'fire_occurrences' is 1 and 0
+fire_occurrences_one = env_data[env_data['fire_occurrence'] == 1]
+fire_occurrences_zero = env_data[env_data['fire_occurrence'] == 0]
+
+# Randomly sample num_non_fires rows from those where 'fire_occurrences' is 0
+sampled_rows = fire_occurrences_zero.sample(n=num_non_fires, random_state=42)
+
+# Append the sampled rows to the rows where 'fire_occurrences' is 1
+env_data = pd.concat([fire_occurrences_one, sampled_rows])
+
+# Shuffle the combined data (if you want random order)
+env_data = env_data.sample(frac=1).reset_index(drop=True)
+
 # print(env_data)
 
 X = env_data.drop(columns=['fire_occurrence', 'latitude', 'longitude', 'timestamp'])
 y = env_data['fire_occurrence']
 
+print(X)
+
 num_runs = 5
 total_accurracy = 0
+total_f1 = 0
 for _ in range(num_runs):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
@@ -57,11 +78,15 @@ for _ in range(num_runs):
 
     accuracy = accuracy_score(y_test, y_pred)
     conf_matrix = confusion_matrix(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
     print("Accuracy:", accuracy)
     print("Confusion Matrix:\n", conf_matrix)
+    print(f"F1-score: {f1}")
 
     total_accurracy += accuracy
+    total_f1 += f1
 
-print('Avergae Accuracy over', num_runs, ': ', total_accurracy / num_runs)
+print('Avergae accuracy over', num_runs, ': ', total_accurracy / num_runs)
+print('Avergae f1 score over', num_runs, ': ', total_f1 / num_runs)
 
